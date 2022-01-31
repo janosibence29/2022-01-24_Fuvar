ccbf7e7e59338f4b81eabdb677e03b2887e5dba1
janosibence29 <92314900+janosibence29@users.noreply.github.com>
2022-01-31 09:54:33 +0100

    1-4

---
+++ 2022-01-24_Fuvar/Fuvar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022_01_24_Fuvar
+{
+    class Fuvar
+    {
+        public int TaxiId { get; set; }
+        public DateTime Indulás { get; set; }
+        public int Időtartam { get; set; }
+        public double Távolság { get; set; }
+        public double Viteldíj{ get; set; }
+        public double Borravaló { get; set; }
+        public string FizetésMód { get; set; }
+
+        public Fuvar(string sor)
+        {
+            string[] t = sor.Split(';');
+            TaxiId = int.Parse(t[0]);
+            Indulás = DateTime.Parse(t[1]);
+            Időtartam = int.Parse(t[2]);
+            Távolság = double.Parse(t[3]);
+            Viteldíj = double.Parse(t[4]);
+            Borravaló = double.Parse(t[5]);
+            FizetésMód =t[6];
+        }
+    }
+}
--- 2022-01-24_Fuvar/Program.cs
+++ 2022-01-24_Fuvar/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace _2022_01_24_Fuvar
 {
@@ -10,7 +11,26 @@ namespace _2022_01_24_Fuvar
     {
         static void Main(string[] args)
         {
+            List<Fuvar> fuvarok = new List<Fuvar>();
 
+            foreach (var sor in File.ReadAllLines("fuvar.csv").Skip(1))
+            {
+                fuvarok.Add(new Fuvar(sor));
+            }
+
+            Console.WriteLine($"3. feladat: {fuvarok.Count} fuvar");
+
+            double Bevétel = 0;
+            int db = 0;
+            foreach (var f in fuvarok)
+            {
+                if (f.TaxiId == 6185)
+                {
+                    Bevétel += f.Viteldíj + f.Borravaló;
+                    db++;
+                }
+            }
+            Console.WriteLine($"4. feladat: {db} fuvar alatt: {Bevétel}");
             Console.ReadKey();
         }
     }
